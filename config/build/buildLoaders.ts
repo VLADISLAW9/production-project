796dd95c6817b5b893136b24ba09c63bf41425cc
VLADISLAW9 <80681837+VLADISLAW9@users.noreply.github.com>
2023-08-31 17:17:10 +0700

    add config

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,33 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import webpack from 'webpack'
+import { BuildOptions } from './types/config'
+
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+	const cssLoader = {
+		test: /\.s[ac]ss$/i,
+		use: [
+			isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+			{
+				loader: 'css-loader',
+				options: {
+					modules: {
+						auto: (resPath: string) =>
+							Boolean(resPath.includes('.module.scss')),
+						localIdentName: isDev
+							? '[path][name]__[local]--[hash:base64:5]'
+							: '[hash:base64:8]',
+					},
+				},
+			},
+			'sass-loader',
+		],
+	}
+
+	const typescriptLoader = {
+		test: /\.tsx?$/,
+		use: 'ts-loader',
+		exclude: /node_modules/,
+	}
+
+	return [typescriptLoader, cssLoader]
+}
---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,29 @@
+import { BuildOptions } from './types/config'
+import webpack from 'webpack'
+import { buildPlugins } from './buildPlugins'
+import { buildLoaders } from './buildLoaders'
+import { buildResolves } from './buildResolves'
+import path from 'path'
+import { buildDevServer } from './buildDevServer'
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+
+	const {paths, mode, isDev} = options;
+
+	return {
+		mode: mode,
+		entry: paths.entry,
+		output: {
+			filename: '[name].[contenthash].js',
+			path: paths.build,
+			clean: true,
+		},
+		plugins: buildPlugins(options),
+		module: {
+			rules: buildLoaders(options),
+		},
+		resolve: buildResolves(),
+		devtool: isDev ? 'inline-source-map' : undefined,
+		devServer: isDev ? buildDevServer(options) : undefined
+	}
+}
