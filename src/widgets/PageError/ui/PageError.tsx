9a4f9e18d28e6825166c42025e8a3ad6bc367776
VLADISLAW9 <80681837+VLADISLAW9@users.noreply.github.com>
2023-09-17 15:56:07 +0700

    fix error linter

--- src/widgets/PageError/ui/PageError.tsx
+++ src/widgets/PageError/ui/PageError.tsx
@@ -1,27 +1,26 @@
-import { useTranslation } from 'react-i18next'
-import { classNames } from 'shared/lib/classNames/classNames'
-import { Button } from 'shared/ui/Button/Button'
-import cls from './PageError.module.scss'
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button/Button';
+import cls from './PageError.module.scss';
 
 interface PageErrorProps {
-	className?: string
+   className?: string
 }
 
 export const PageError = ({ className }: PageErrorProps) => {
+    const { t } = useTranslation();
 
-	const { t } = useTranslation()
+    const reloadPage = () => {
+        // eslint-disable-next-line no-restricted-globals
+        location.reload();
+    };
 
-	const reloadPage = () => {
-		// eslint-disable-next-line no-restricted-globals
-		location.reload()
-	}
-
-	return (
-		<div className={classNames(cls.PageError, {}, [className])}>
-			<p>{t('Произошла непредвиденная ошибка')}</p>
-			<Button onClick={reloadPage}>
-				{t('Обновить страницу')}
-			</Button>
-		</div>
-	)
-}
+    return (
+        <div className={classNames(cls.PageError, {}, [className])}>
+            <p>{t('Произошла непредвиденная ошибка')}</p>
+            <Button onClick={reloadPage}>
+                {t('Обновить страницу')}
+            </Button>
+        </div>
+    );
+};
--- src/widgets/PageLoader/ui/PageLoader.tsx
+++ src/widgets/PageLoader/ui/PageLoader.tsx
@@ -1,15 +1,13 @@
-import { classNames } from 'shared/lib/classNames/classNames'
-import { Loader } from 'shared/ui/Loader/Loader'
-import cls from './PageLoader.module.scss'
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Loader } from 'shared/ui/Loader/Loader';
+import cls from './PageLoader.module.scss';
 
 interface PageLoaderProps {
-	className?: string
+   className?: string
 }
 
-export const PageLoader = ({ className }: PageLoaderProps) => {
-	return (
-		<div className={classNames(cls.PageLoader, {}, [className])}>
-			<Loader />
-		</div>
-	)
-}
+export const PageLoader = ({ className }: PageLoaderProps) => (
+    <div className={classNames(cls.PageLoader, {}, [className])}>
+        <Loader />
+    </div>
+);
