a1e42e7a8b667a7db0d6d18620e2bf3fd2bad7ea
VLADISLAW9 <80681837+VLADISLAW9@users.noreply.github.com>
2023-10-08 19:53:23 +0700

    fix screenshots

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from '../LoginForm/LoginForm';
-import cls from './LoginModal.module.scss';
 
 interface LoginModalProps {
    className?: string;
@@ -13,7 +12,7 @@ export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => {
     const i = 1;
     return (
         <Modal
-            className={classNames(cls.LoginModal, {}, [className])}
+            className={classNames('', {}, [className])}
             isOpen={isOpen}
             onClose={onClose}
             lazy
