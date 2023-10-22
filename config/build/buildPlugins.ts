1dc91c01fae7e1e884b8fd1930cca2384888e1b9
VLADISLAW9 <80681837+VLADISLAW9@users.noreply.github.com>
2023-10-22 21:20:30 +0700

    bug fix

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -22,14 +22,13 @@ export function buildPlugins({
         }),
     ];
 
-    plugins.push(
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
-    );
-
     if (isDev) {
         plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(
+            new BundleAnalyzerPlugin({
+                openAnalyzer: false,
+            }),
+        );
     }
 
     return plugins;
--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -4,8 +4,12 @@ import { userReducer } from 'entities/User';
 import { createReducerManager } from './reducerManager';
 import { StateSchema } from './StateSchema';
 
-export function createReduxStore(initialState?: StateSchema) {
+export function createReduxStore(
+    initialState?:StateSchema,
+    asyncReducers?: ReducersMapObject<StateSchema>,
+) {
     const rootReducers: ReducersMapObject<StateSchema> = {
+        ...asyncReducers,
         counter: counterReducer,
         user: userReducer,
     };
