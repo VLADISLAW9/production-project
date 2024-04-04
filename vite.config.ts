03e649dce5fc8c028bd6b72be8d399c928c77ca7
VLADISLAW9 <80681837+VLADISLAW9@users.noreply.github.com>
2024-04-04 15:00:40 +0700

    add vite for dev mode

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import svgr from "vite-plugin-svgr";
+import  react  from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [
+		react(), 
+		svgr({exportAsDefault: true})
+	],
+	resolve: {
+		alias: [
+			{find: '@', replacement: '/src'}
+		]
+	},
+	define: {
+		__IS_DEV__: JSON.stringify(true),
+		__API__: JSON.stringify('http://localhost:8000'),
+		__PROJECT__: JSON.stringify('frontend'),
+	}
+})
