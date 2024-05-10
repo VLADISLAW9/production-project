bbd7262918dc0b3fe29fed62dc2b97e87e2aec09
VLADISLAW9 <80681837+VLADISLAW9@users.noreply.github.com>
2024-05-10 16:34:15 +0700

    aadd greeting modal for articles page

--- src/entities/User/model/types/jsonSettings.ts
+++ src/entities/User/model/types/jsonSettings.ts
@@ -2,6 +2,6 @@ import { Theme } from '@/shared/const/theme';
 
 export interface JsonSettings {
     theme?: Theme;
-    isFirstVisit?: true;
-    settingPageHasBeenOpen?: false;
+    isFirstVisit?: boolean;
+    isArticlePageWasOpened?: boolean;
 }
